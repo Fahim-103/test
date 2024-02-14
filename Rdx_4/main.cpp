65dad227ce1f65cf9950851fff17f0d5e4f9f964
Fahim-103 <127331206+Fahim-103@users.noreply.github.com>
2024-02-14 18:45:42 -0500

    Add files via upload

--- Rdx_4/main.cpp
+++ Rdx_4/main.cpp
@@ -1,44 +1,72 @@
 #include <iostream>
 using namespace std;
 
+uint32_t   mdl (uint64_t   t, uint32_t   w, uint32_t   q) {
+
+    __uint128_t  w_con = (uint64_t ) 1 << 32;
+    printf ("%llu \n", w_con );
+    w_con = w_con/65537;   //// store this 255
+    printf ("w_con/q: %u \n", w_con );
+
+
+    //uint32_t  tt = w*t - q * ((w_con*t) >> 16);
+
+    uint64_t  tt = t - q * ((65535 * t) >> 32);   //////////////////////////
+
+
+    return tt;
+
+}
+
+
 int main() {
 
-    int e[4] = {7497,	2371,	2885,	5195};
-    int wtt = 5756;
-    int wq  = 7681;
+    uint32_t   e[4] = {4,3,2,1};
+    uint32_t   wtt = 32;
+    uint32_t   wq  = 65537;
 
-    int a = e[0];
-    int b = e[1];
-    int c = e[2];
-    int d = e[3];
+    uint32_t   a = e[0];
+    uint32_t   b = e[1];
+    uint32_t   c = e[2];
+    uint32_t   d = e[3];
 
 
-    int t0 = (  (a + c)  +      (b + d)  +wq ) % wq ;
-    int t1 = (  (a - c)  + (wtt*(b - d)) +wtt*wq ) % wq ;
-    int t2 = (  (a + c)  -      (b + d)  +wq ) % wq ;
-    int t3 = (  (a - c)  - (wtt*( b - d)) +wtt*wq ) % wq ;
+    uint32_t   t0 = (  (a + c)  +      (b + d)  )  ;
 
-    /* int t0 = (  (a + c)  +      (b + d)  )  ;
+    uint32_t   t1 = (  (a - c)  + (wtt*(b - d)) )  + wq*(wtt+1);
+
+    uint32_t   tn = t1 % wq;
+    uint32_t   tnn = mdl(t1, 1, wq);
+    printf(" %d %d %d\n", t1, tn, tnn);
+
+    uint32_t   t2 = (  (a + c)  -      (b + d)  ) + 2*wq ;
+
+    uint32_t   tn2 = t2 % wq;
+    uint32_t   tnn2 = mdl(t2, 1, wq);
+    printf(" %d %d %d\n", t2, tn2, tnn2);
+
+
+    uint32_t   t3 = (  (a - c)  - (wtt*( b - d))  ) + wq*(wtt+1);
+
+    uint32_t   tn3 = t3 % wq;
+    uint32_t   tnn3 = mdl(t3, 1, wq);
+    printf(" %d %d %d\n", t3, tn3, tnn3);
+
+
+    /* uint32_t   t0 = (  (a + c)  +      (b + d)  )  ;
      //t0 -= (int) (t0/wq);
-     int t1 = (  (a - c)  + (wtt*((b - d) +wq ) +wq) )  ;
-     int t2 = (  (a + c)  -      (b + d) + wq  )  ;
-     int t3 = (  (a - c)  - (wtt*(( b - d)) + wq ) );*/
+     uint32_t   t1 = (  (a - c)  + (wtt*((b - d) +wq ) +wq) )  ;
+     uint32_t   t2 = (  (a + c)  -      (b + d) + wq  )  ;
+     uint32_t   t3 = (  (a - c)  - (wtt*(( b - d)) + wq ) );*/
 
     ///////////////////modulo operation///////////////
-    int p = 323;
-    int q = 3;
 
-    int r = (int) p/q;
-
-    int v = p - r*q + q;
-
-    int w = v - (int)(v/q)*q;
-
-    printf("%d %d \n", v, w);
 
     //////////////////////////////////////////////
 
     printf ("%d %d %d %d\n", t0, t1, t2, t3);
+
+
 
 
 
